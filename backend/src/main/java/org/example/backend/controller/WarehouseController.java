18db7509e63926385f96b2de998ec2e43919e7f1
Sushmita <ajagannanavars@gmail.com>
2025-11-24 15:12:52 +0100

    created and updated controller class

--- backend/src/main/java/org/example/backend/controller/WarehouseController.java
+++ backend/src/main/java/org/example/backend/controller/WarehouseController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/example")
+@RequestMapping("/api/warehouse")
 public class WarehouseController {
 }

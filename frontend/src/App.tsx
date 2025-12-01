550e2b59d5cb6a3891d5d97416321358eb1f4d9e
Sushmita <ajagannanavars@gmail.com>
2025-12-01 17:33:41 +0100

    add Dockerfile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,27 @@
+import {useState} from "react";
+import axios from "axios";
+
 export default function App(){
-    return( <>
-        <h1>hello</h1>
+    const [userName, setUserName] = useState<string>("")
+
+    function login(){
+        const host:string = window.location.host ==="localhost:5173" ? "http://localhost:8080" : window.location.origin
+
+        window.open(host + "/oauth2/authorization/github", "_self")
+    }
+
+    function getMe(){
+       axios.get("/api/users")
+           .then(r => setUserName(r.data))
+           .catch(e => console.log(e.message))
+    }
+
+    return(
+        <>
+        <h1>Login Page</h1>
+            <h2>{userName}</h2>
+            <button onClick={login}>Login</button>
+            <button onClick={getMe}>getMe!</button>
         </>);
 }
 
